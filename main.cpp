bb40f1b648543621e36c80c6e55ba103b83f9864
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-03-27 21:54:17 -0300

    indetificacao part2

--- main.cpp
+++ main.cpp
@@ -3,12 +3,21 @@
 int main(){
 
    std::string nome;
+    int idade;
+    float altura;
 
    std::cout<<"Qual seu nome:"<<std::endl;
    std::cin>>nome;
-
-    std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
-
+   
+   std::cout<<"Qual e sua idade"<<std::endl;
+   std::cin>>idade;
+   
+   std::cout<<"Qual e sua altura"<<std::endl;
+   std::cin>>altura;
+    
+   std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
+    std::cout<<"Sua idade e "<<idade<<"!!"<<std::endl;
+    std::cout<<"Sua altura e "<<altura<<"!!"<<std::endl;
 
     return 0;
 }
