acf5b28648799ac61af692c7a2c41e530e4b1744
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-03-30 19:55:42 -0300

    semanas dias

--- main.cpp
+++ main.cpp
@@ -2,22 +2,15 @@
 #include<string>
 int main(){
 
-   std::string nome;
-    int idade;
-    float altura;
+   std::string diaDaSemana;
+   
+   
+   std::cout<<"Que dia é hoje:"<<std::endl;
+   std::cin>>diaDaSemana;
 
-   std::cout<<"Qual seu nome:"<<std::endl;
-   std::cin>>nome;
+   std::cout<<"Hoje é "<<diaDaSemana<<"!!"<<std::endl;
    
-   std::cout<<"Qual e sua idade"<<std::endl;
-   std::cin>>idade;
-   
-   std::cout<<"Qual e sua altura"<<std::endl;
-   std::cin>>altura;
+
     
-   std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
-    std::cout<<"Sua idade e "<<idade<<"!!"<<std::endl;
-    std::cout<<"Sua altura e "<<altura<<"!!"<<std::endl;
-
     return 0;
 }
