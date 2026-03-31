110f533d7f91c76de40742088a90df23163cfe52
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-03-30 22:00:49 -0300

    operador logico

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 int main(){
 
     int a = 10;
+    int b = 6;
 
-    std::cout<<++a<<std::endl;
-    std::cout<<++a<<std::endl;
+    std::cout<<((a > b) && (a + b) >= 16)<<std::endl;
 
     return 0;
 }
