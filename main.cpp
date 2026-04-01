f38f6878bf4cba57a642adcf6eed8d5d4e9df1b6
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-01 19:33:40 -0300

    apostrufo

--- main.cpp
+++ main.cpp
@@ -3,11 +3,9 @@
 
 int main(){
 
-    bool condicao = true;
-    bool condicao1 = false;
+    char letra = 'E';
 
-    std::cout<<condicao<<std::endl;
-     std::cout<<condicao1<<std::endl;
+    std::cout<<letra<<std::endl;
 
     return 0;
 }
