53a842957643fb879ca2f94a238877005b3517a7
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-01 19:49:09 -0300

    teste de auto

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
-#include<string>
+
 
 int main(){
 
-    char letra = 'E';
+    auto num1 = 10;
+    auto num2 = 15.80f;
+    auto letra ='E';
+    auto texto = "Theodoro";        
 
+    std::cout<<num1<<std::endl;
+    std::cout<<num2<<std::endl;
     std::cout<<letra<<std::endl;
+    std::cout<<texto<<std::endl;
 
     return 0;
 }
