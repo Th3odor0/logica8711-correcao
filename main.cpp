2737788430cfefa4ba6d8b53910d51151b786bc8
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-08 20:51:47 -0300

    logica com max

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
 
 int main(){
 
-   int a = 5;
-   int b = 3;
-   
-   std::cout<<std::max(a, b)<<std::endl;
-    
+  int a = 5;
+  int b = 3;
+    int c =5;
+  int d =2;
 
+  int maior1 = std::max(a, b);
+  int maior2 = std::max(c, d);
+
+  std::cout<<(maior1 > maior2)<<std::endl;
+  
 
     return 0;
 }
