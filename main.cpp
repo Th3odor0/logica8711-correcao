2ad82b89ac6d8e8eb7c803fce085538a568021f2
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-08 21:38:17 -0300

    if else

--- main.cpp
+++ main.cpp
@@ -4,9 +4,16 @@
 
 int main(){
 
-  std::cout<<std::sqrt(64)<<std::endl;
-  std::cout<<std::round(2.6)<<std::endl;
-  std::cout<<std::log(2)<<std::endl;
+  int a = 5;
+  int b = 2;
+
+  if (a > b)
+  {
+    std::cout<<"A é maior que B. "<<std::endl;
+  }else{
+    std::cout<<"B é maior que A. "<<std::endl;
+  }
+  
 
     return 0;
 }
