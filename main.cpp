9de9a197d5decf325d8bfddb5f1d24d6258e93bb
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-13 19:46:50 -0300

    chore: exercício de if/else com múltiplas condições

--- main.cpp
+++ main.cpp
@@ -7,12 +7,21 @@ int main(){
     std::cout<<"Digite a sua idade: "<<std::endl;
     std::cin>>idade;
 
-    if (idade >= 18)
-    {std::cout<<"Voce e maior de idade " <<std::endl;
-        /* code */
+    if (idade < 0)
+    {std::cout<<"Idade invalida! " <<std::endl;
+        
+    }else if(idade <= 2 ){
+        std::cout<<"Voce e um bebe!"<<std::endl;
+    }else if(idade <= 8){
+        std::cout<<"voce e uma criança!"<<std::endl;
+    }else if(idade <= 16){
+        std::cout<<"voce e um adolesente"<<std::endl;
+    }else if(idade <= 60){
+        std::cout<<"voce e adulto(a)"<<std::endl;
     }else{
-        std::cout<<"Voce e menor de idade!"<<std::endl;
+        std::cout<<"voce e um(a) senior"<<std::endl;
     }
+
     
     
 
