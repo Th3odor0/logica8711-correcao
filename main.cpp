aa39ff5cdd7dcb41fb3ff77bf181c4fcdcdd8712
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-15 19:16:07 -0300

    feat: utiliza loop for para repetir saída no console 10 vezes

--- main.cpp
+++ main.cpp
@@ -3,34 +3,9 @@
 
 int main(){
 
-    std::string nome;
-    int idade;
-    float altura;
-
-   std::cout<<"Qual seu nome:"<<std::endl;
-   std::cin>>nome;
-
-   std::cout<<"Qual e sua idade"<<std::endl;
-   std::cin>>idade;
-   
-   std::cout<<"Qual e sua altura"<<std::endl;
-   std::cin>>altura;
-    
-   std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
-    std::cout<<"Sua idade e "<<idade<<"!!"<<std::endl;
-    std::cout<<"Sua altura e "<<altura<<"!!"<<std::endl;
-
-    if (idade < 0)
-    {std::cout<<"Idade invalida! " <<std::endl; 
-    }else if(idade <= 10 ){
-        std::cout<<"Voce e um(a) crianca!"<<std::endl;
-    }else if(idade <= 18){
-        std::cout<<"voce e um adolesente"<<std::endl;
-    }else if(idade <= 60){
-        std::cout<<"voce e adulto(a)"<<std::endl;
-    }else{
-        std::cout<<"voce e um(a) senior"<<std::endl;
-    }
+   for(int l = 1; l <= 10; l++){
+    std::cout<<"Este e um loop!!"<<std::endl;
+   }
 
     
     
