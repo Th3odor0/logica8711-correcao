1e49d22018c25cb989f03679a05acd48eb32fe74
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-15 20:25:49 -0300

    implemento de dia da semana com swicht

--- main.cpp
+++ main.cpp
@@ -4,11 +4,36 @@
 
 int main(){
 
-    int tabuada = 7;
+    int opcao;
 
-    for (int i = 0; i <= 10; i++)
+    std::cout<<"Digite um valor para opcao: "<<std::endl;
+    std::cin>>opcao;
+
+    switch (opcao)
     {
-        std::cout<<"O resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
+    case 1:
+        std::cout<<"domingo"<<std::endl;
+        break;
+        case 2:
+    std::cout<<"segunda-feira"<<std::endl;
+   break;
+    case 3:
+    std::cout<<"terca-feira"<<std::endl;
+    break;
+    case 4:
+    std::cout<<"quarta-feira"<<std::endl;
+    break;
+    case 5:
+    std::cout<<"quinta-feira"<<std::endl;
+    break;
+    case 6:
+    std::cout<<"sexta-feira"<<std::endl;
+    break;
+    case 7:
+    std::cout<<"sabado"<<std::endl;
+        break;
+    default:
+    std::cout<<"numero invalido"<<std::endl;
     }
     
 
