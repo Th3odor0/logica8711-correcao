f10644254c93b7c3e3365b146933537dd9012ff4
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-15 19:45:30 -0300

    teste de segundos falhos

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
-#include<string>
+#include<thread>
+#include<chrono>
 
 int main(){
 
    for(int hiroshima = 10; hiroshima >= 0; hiroshima--){
     std::cout<<"a hiroshima ira explodir em..."<<hiroshima<<std::endl;
+    
+     std::this_thread::sleep_for(std::chrono::microseconds(500));
    }
 
-    
-    
 
 
       
