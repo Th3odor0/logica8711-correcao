faf1f6c1fe93c646af207eec66e72e6b24e44808
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-15 21:01:13 -0300

    uso do acumulador com for

--- main.cpp
+++ main.cpp
@@ -4,38 +4,13 @@
 
 int main(){
 
-    int opcao;
+    int soma = 0;
 
-    std::cout<<"Digite um valor para opcao: "<<std::endl;
-    std::cin>>opcao;
-
-    switch (opcao)
-    {
-    case 1:
-        std::cout<<"domingo"<<std::endl;
-        break;
-        case 2:
-    std::cout<<"segunda-feira"<<std::endl;
-   break;
-    case 3:
-    std::cout<<"terca-feira"<<std::endl;
-    break;
-    case 4:
-    std::cout<<"quarta-feira"<<std::endl;
-    break;
-    case 5:
-    std::cout<<"quinta-feira"<<std::endl;
-    break;
-    case 6:
-    std::cout<<"sexta-feira"<<std::endl;
-    break;
-    case 7:
-    std::cout<<"sabado"<<std::endl;
-        break;
-    default:
-    std::cout<<"numero invalido"<<std::endl;
+    for(int i = 1; i <= 5; i++){
+        soma = soma + i;
     }
     
+    std::cout<<"soma e: "<<soma<<std::endl;
 
 
 
