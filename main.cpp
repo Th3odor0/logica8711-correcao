c4ff0b285c12327b58720ab259d1349bdd061a34
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-15 21:22:36 -0300

    chore: prática com loops aninhados

--- main.cpp
+++ main.cpp
@@ -4,13 +4,17 @@
 
 int main(){
 
-    int soma = 0;
+    
 
-    for(int i = 1; i <= 5; i++){
-        soma = soma + i;
+    for(int i = 1; i <= 2; ++i){
+        std::cout<<"externo: "<<i<<std::endl;
+
+        for(int j = 1; j <= 3; ++j){
+            std::cout<<"interno: "<<j<<std::endl;
+        }
+        
     }
     
-    std::cout<<"soma e: "<<soma<<std::endl;
 
 
 
