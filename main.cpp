732382af6a2dd11beea60d8e91027726ceb9a7b0
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-15 21:47:38 -0300

    aprendendo for each em array

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
-#include<thread>
-#include<chrono>
+#include<windows.h> 
 
 int main(){
 
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
     int myNumbers[5] = {10, 20, 30, 40, 50};
 
-    for(int num : myNumbers){
-        std::cout<<num<<std::endl;
+    // for(int num : myNumbers){
+    //     std::cout<<num<<std::endl;
         
-    }
+        
+
+        for(int i = 0; i < 5; ++i){
+            std::cout<<myNumbers[i]<<std::endl;
+        }
+        
+    
     
 
 
