593a31d54c619a0c00127e7cf3caadb03cced615
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-20 21:52:13 -0300

    valida entrada com while

--- main.cpp
+++ main.cpp
@@ -8,16 +8,18 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-  int contagem = 10;
+    int num;
 
-  while (contagem > 0)
-  {
-    std::cout<<contagem<<std::endl;
-    contagem--;
-  }
+    do{
+        std::cout<<"Digite um valor para num: "<<std::endl;
+        std::cin>>num;
   
-  std::cout<<"Feliz ano novo!!"<<std::endl;
-
+        if(num < 1 || num > 10){
+            std::cout<<"Entrada invalida!!"<<std::endl;
+        }
+    }while(num < 1 || num > 10);{
+        std::cout<<"Obrigado!! você escolheu o número: "<<num<<std::endl;
+    }
       
     return 0;
 }
