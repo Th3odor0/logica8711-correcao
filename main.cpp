d4aada5038f2b332c73f2f9fb2e74162fb46ff62
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-28 20:15:42 -0300

    aprendendo a usar void

--- main.cpp
+++ main.cpp
@@ -3,21 +3,16 @@
 #include<string>
 #include<vector>
 
+void myFunction(){
+    std::cout<<"Olá mundo!"<<std::endl;
+}
+
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int myNumbers[5] = {1, 2, 3, 4, 5};
-
-    //for (int i = 0; i < 5; i++){
-      //  std::cout<<myNumbers[i]<<std::endl;
-    //}
-    
-    for (int num : myNumbers)
-    {
-        std::cout<<num<<std::endl;
-    }
+   myFunction();
     
     
       
