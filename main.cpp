fe243cd750b8db8be89ea2cb1478d38549a83deb
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-28 20:01:02 -0300

    imprime elementos de array com for

--- main.cpp
+++ main.cpp
@@ -10,11 +10,14 @@ int main(){
 
     int myNumbers[5] = {1, 2, 3, 4, 5};
 
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout<<myNumbers[i]<<std::endl;
-    }
+    //for (int i = 0; i < 5; i++){
+      //  std::cout<<myNumbers[i]<<std::endl;
+    //}
     
+    for (int num : myNumbers)
+    {
+        std::cout<<num<<std::endl;
+    }
     
     
       
