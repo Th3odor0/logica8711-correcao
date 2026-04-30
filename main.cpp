4f0388c8dc33b17ef9f7882e19f922b5049ba283
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-04-30 19:53:51 -0300

    dicionar conversor de temperatura

--- main.cpp
+++ main.cpp
@@ -7,24 +7,27 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-   int opcao;
-   std::string nome;
-   int idade;
+  int choice;
+  float temp, converted;
 
-   std::cout<<"----Escolha a opção adequada: "<<std::endl;
-   std::cout<<"Digite 1 para cadastrar ou 2 para sair: "<<std::endl;
-   std::cin>>opcao;
+   std::cout<<"---- Temperatura converter ----"<<std::endl;
+   std::cout<<"1: celsius to fahrenheit"<<std::endl;
+   std::cout<<"2: fahrenheit to celsius"<<std::endl;
+   std::cout<<"choose an option (1 or 2): "<<std::endl;
+   std::cin>>choice;
 
-   if (opcao == 1){
-    std::cout<<"Digite o nome: "<<std::endl;
-    std::cin>>nome;
-    std::cout<<"Digite a idade"<<std::endl;
-    std::cin>>idade;
-
-    std::cout<<"Seu nome é: "<<nome<<std::endl;
-    std::cout<<"Sua idade é: "<<idade<<std::endl;
+   if (choice == 1){
+    std::cout<<"Enter temperatura in celsius: "<<std::endl;
+    std::cin>>temp;
+    converted = (temp * 1.8) + 32;
+    std::cout<<temp<<"C is "<<converted<<" F. "<<std::endl;
+   }else if (choice == 2){
+    std::cout<<"Enter temparatura in Fahrenheit: "<<std::endl;
+    std::cin>>temp;
+    converted = (temp - 32) / 1.8;
+    std::cout<<temp<<"F is "<<converted<<" C. "<<std::endl;
    }else{
-    std::cout<<"Até a próxima!"<<std::endl;
+    std::cout<<"Invalid option! Please restart."<<std::endl;
    }
    
     
