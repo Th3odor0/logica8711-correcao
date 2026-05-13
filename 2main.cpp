889e9550d29cdee8e76273ccfb0d3e1cd1f441c4
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-05-12 21:55:59 -0300

    exemplos de while do

--- 2main.cpp
+++ 2main.cpp
@@ -1,68 +1,27 @@
 #include<iostream>
 #include<Windows.h>
 
-double saldo = 1000.9;
-std::string titular = "Guilherme Paz";
 
-void exibirSaldo(){
-    std::cout<<" --- Saldo: R$ "<<saldo<<std::endl;
-}
-
-void depositar(double valor){
-    if(valor > 0){
-        saldo += valor;
-        std::cout<<"Deposito reazlizado com sucesso!"<<std::endl;
-    }else{
-        std::cout<<"Valor inválido!"<<std::endl;
-    }
-}
-
-void sacar(double valor){
-    if(valor > 0 && valor <= saldo){
-        saldo += valor;
-        std::cout<<"Saldo insuficiente ou valor inválido!"<<std::endl;
-    }
-}
-
-void exibirMenu(){
-    std::cout<<"\n === MENU CONTA BANCÁRIA ==="<<std::endl;
-    std::cout<<"1 - Ver saldo"<<std::endl;
-    std::cout<<"2 - Depósito"<<std::endl;
-    std::cout<<"3 - Sacar"<<std::endl;
-    std::cout<<"4 - Sair"<<std::endl;
-    std::cout<<"Digite uma opção: ";
-}
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int opcao;
-    double valor;
+   int numero, sorteado = 42;
 
-    do{
-        exibirMenu();
-        std::cin>>opcao;
+   do{
+    std::cout<<"Advinhe: ";
+    std::cin>>numero;
+    if (numero < sorteado){ 
+    std::cout<<"Maior!"<<std::endl;
+    }else if (numero > sorteado)
+     std::cout<<"Menor!"<<std::endl;
+    }while (numero != sorteado);
 
-        switch(opcao){
-            case 1:
-            exibirSaldo();
-            break;
-            case 2:
-            std::cout<<"Digite o valor a depositar: "<<std::endl;
-            std::cin>>valor;
-            break;
-            case 3:
-            std::cout<<"Digite o valor a sacar: "<<std::endl;
-            std::cin>>valor;
-            sacar(valor);
-            case 4:
-            std::cout<<"Saindo da conta.."<<std::endl;
-            break;
-            default:
-            std::cout<<"Opção inválida!"<<std::endl;
-        }
-    }while(opcao != 4);
+   std::cout<<"Acertou! "<<std::endl;
+   
+
+   
 
     return 0;
 }
