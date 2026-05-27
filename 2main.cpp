1e8865cf5224edd7f5f5a265fb9b4faf0eeffbda
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-05-26 21:33:16 -0300

    função de encontrar maior numero com array

--- 2main.cpp
+++ 2main.cpp
@@ -2,22 +2,34 @@
 #include<Windows.h>
 
 
+int encontrarMaior(int arr[], int tamanho){
+    int maior = arr[0];
+
+    for (int i = 0; i < tamanho; i++){
+        if (arr[i] > maior){
+            maior = arr[i];
+        }
+        
+    }
+return maior;    
+}
+
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    
     int numeros[5];
-    int soma = 0;
 
-    for(int i = 1; i <= 5; i++){
-        std::cout<<"Digite o numero: "<<(i + 1)<<":";
-        std::cin>>numeros[i];
-        soma += numeros[i];
-    }
+    std::cout<<"Digite 5 numeros: "<<std::endl;
+        for (int i = 0; i < 5; i++){
+            std::cout<<"Digite o numero "<<(i + 1)<<":";
+            std::cin>>numeros[i];
+        }
 
-    std::cout<<"Soma: "<<soma<<std::endl;
+        std::cout<<"Maior numero: "<<encontrarMaior(numeros, 5)<<std::endl;
+        
+
 
     return 0;
 }
