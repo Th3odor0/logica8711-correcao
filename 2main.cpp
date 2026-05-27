efb48a1822d43fdb39764609a0886929dc7b624e
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-05-26 21:09:08 -0300

    revendo conceitos de if

--- 2main.cpp
+++ 2main.cpp
@@ -1,23 +1,23 @@
 #include<iostream>
 #include<Windows.h>
 
-int quadrado(int n){
-    return n * n;
-}
-
-int cubo(int c){
-    return c * c * c;
-}
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::cout<<"Quadrado de 5: "<<quadrado(5)<<"cubo de 5"<<cubo(3)<<std::endl;
-    std::cout<<"Quadrado de 8: "<<quadrado(8)<<"cubo de 8"<<cubo(6)<<std::endl;
-    std::cout<<"Quadrado de 10: "<<quadrado(10)<<"cubo de 10"<<cubo(9)<<std::endl;
+    
+    int numeros[5];
+    int soma = 0;
 
+    for(int i = 1; i <= 5; i++){
+        std::cout<<"Digite o numero: "<<(i + 1)<<":";
+        std::cin>>numeros[i];
+        soma += numeros[i];
+    }
+
+    std::cout<<"Soma: "<<soma<<std::endl;
 
     return 0;
 }
