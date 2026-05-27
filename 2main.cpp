aab26f34f79eda9b9561802238d4db50a44766fe
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-05-26 21:45:54 -0300

    feat: implementa algoritmo para identificar números primos

--- 2main.cpp
+++ 2main.cpp
@@ -2,16 +2,17 @@
 #include<Windows.h>
 
 
-int encontrarMaior(int arr[], int tamanho){
-    int maior = arr[0];
-
-    for (int i = 0; i < tamanho; i++){
-        if (arr[i] > maior){
-            maior = arr[i];
+bool ehPrimo(int numero){
+    if(numero <= 1){
+        return false;
+    }
+    for (int i = 2; i < numero; i++){
+        if (numero % i == 0){
+            return false;
         }
         
     }
-return maior;    
+    return true;
 }
 
 int main(){
@@ -19,16 +20,17 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int numeros[5];
+    int numero;
 
-    std::cout<<"Digite 5 numeros: "<<std::endl;
-        for (int i = 0; i < 5; i++){
-            std::cout<<"Digite o numero "<<(i + 1)<<":";
-            std::cin>>numeros[i];
-        }
-
-        std::cout<<"Maior numero: "<<encontrarMaior(numeros, 5)<<std::endl;
-        
+    std::cout<<"Digite um numero: "<<std::endl;
+    std::cin>>numero;
+    
+    if (ehPrimo(numero)){
+        std::cout<<numero<<" é primo"<<std::endl;
+    }else{
+        std::cout<<numero<<" Não é primo"<<std::endl;
+    }
+    
 
 
     return 0;
