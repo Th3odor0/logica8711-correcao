e69eeced4bb30c051771409bbae15316577cef64
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-05-28 19:23:34 -0300

    adiciona exemplo de uso de std::tuple para armazenar dados de uma pessoa

--- 2main.cpp
+++ 2main.cpp
@@ -1,18 +1,33 @@
 #include<iostream>
-#include<Windows.h>
 #include<tuple>
 #include<string>
+#include<Windows.h>
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::tuple<int, std::string, double> aluno(1, "Theodoro", 9.5);
+    std::string nome;
+    int idade;
+    double altura;
 
-    std::cout<<"ID: "<<std::get<0>(aluno)<<"\n";
-    std::cout<<"Nome: "<<std::get<1>(aluno)<<"\n";
-    std::cout<<"Nota: "<<std::get<2>(aluno)<<"\n";
+    std::cout<<"Digite seu nome: "<<std::endl;
+    std::getline(std::cin, nome);
+
+    std::cout<<"Digite a sua idade: "<<std::endl;
+    std::cin>>idade;
+
+    std::cout<<"Digite a sua altura: "<<std::endl;
+    std::cin>>altura;
+
+    std::tuple<std::string, int, double> pessoa = std::make_tuple(nome, idade, altura);
+    
+    std::cout<<"\n ----- Dados Salvos na Tupla -----"<<std::endl;
+
+    std::cout<<"Nome: "<<std::get<0>(pessoa)<<std::endl;
+    std::cout<<"Idade: "<<std::get<1>(pessoa)<<" anos."<<std::endl;
+    std::cout<<"Altura: "<<std::get<2>(pessoa)<<"cm"<<std::endl;
 
     return 0;
 }
