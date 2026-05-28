974d457bcf5445313fe641392b834e85b6ec7a26
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-05-28 20:27:33 -0300

    adiciona logs para rastrear a execução da função recursiva de Fibonacci

--- 2main.cpp
+++ 2main.cpp
@@ -1,24 +1,31 @@
 #include<iostream>
-#include<tuple>
-#include<string>
 #include<Windows.h>
 
-
 int fibonacci(int n){
-    if(n == 0) return 0;
-    if(n == 1) return 1;
+    std::cout<<"calculando fib("<< n<<")"<<std::endl;
 
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    if(n == 0){
+        std::cout<<"fib(0) = 0 (PARADA)"<<std::endl;
+        return 0;
+    }
+    if(n == 1){
+        std::cout<<"Fib(1) = 1 (PARADA)"<<std::endl;
+        return 1;
+    }
+
+    std::cout<<"Fib("<<n<<") = Fib("<<(n-1)<<") + Fib("<<(n-2)<<")\n";
+    int resultado = fibonacci(n -1) + fibonacci(n - 2);
+    std::cout<<"Fib(" << n << ") = "<< resultado << "\n";
+    return resultado;
 }
-
-
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::cout<<fibonacci(6)<<std::endl;
+    fibonacci(4);
+
+
     return 0;
-    
 }
