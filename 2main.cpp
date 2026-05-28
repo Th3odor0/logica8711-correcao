f15fe55b4c4b64855761abbd57de7fd1e4853792
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-05-28 19:57:33 -0300

    adicionando exemplos de fibonacci

--- 2main.cpp
+++ 2main.cpp
@@ -4,14 +4,11 @@
 #include<Windows.h>
 
 
-void contar(int numero){
-    if(numero == 0){
-        std::cout<<"PARE\n";
-        return;
-    }
-    std::cout<<numero<<"\n";
+int fibonacci(int n){
+    if(n == 0) return 0;
+    if(n == 1) return 1;
 
-    contar(numero - 1);
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 
@@ -21,8 +18,7 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    contar(5);
-    
-
+    std::cout<<fibonacci(6)<<std::endl;
     return 0;
+    
 }
