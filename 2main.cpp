bbfc742edc75e3b55fe8d85c074535db66e2dd9d
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-06-02 20:28:58 -0300

    ponteiro da memoria ram

--- 2main.cpp
+++ 2main.cpp
@@ -3,16 +3,6 @@
 #include<string>
 #include<cctype>
 
-bool ehpalidromo(std::string s, int inicio, int fim){
-  if (inicio >= fim){
-    return true;
-  }
-  if (s[inicio] != s[fim]){
-    return false;
-  }
-  return ehpalidromo(s, inicio + 1, fim -1);
-  
-}
 
 
 int main(){
@@ -20,24 +10,14 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::string resp;
+    int numero = 42;
+    int* ptr = &numero;
 
-  
-    
-    std::cout<<"Digite um palindrome"<<std::endl;
-    std::cin>>resp;
+    std::cout<<"Acessar via variavel: "<<numero<<std::endl;
+    std::cout<<"Acessar via ponteiro: "<<*ptr<<std::endl;
+   
 
-    //std::cout<<ehpalidromo(resp)<<std::endl;
-    //puxando a função para dar v ou f 
 
-    for (char &c : resp){
-        c = std::tolower(c);
-    }
-    if (ehpalidromo(resp, 0, resp.length() - 1)){
-        std::cout<<"È palindrome"<<std::endl;
-    }else{
-        std::cout<<"Não é palindrome"<<std::endl;
-    }
 
     return 0;
 }
