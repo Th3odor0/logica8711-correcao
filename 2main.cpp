05a3422b9cf0fa730443a24c9091bb3468c698ad
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-06-02 20:08:57 -0300

    feat: adiciona função recursiva para validar palíndromos

--- 2main.cpp
+++ 2main.cpp
@@ -3,18 +3,15 @@
 #include<string>
 #include<cctype>
 
-bool ehpalidromo(std::string s){
-    int inicio = 0;
-    int fim = s.length() - 1;
-
-    while (inicio < fim){
-        if (s[inicio] != s[fim]){
-            return false;
-        }
-        inicio++;
-        fim--;
-    }
+bool ehpalidromo(std::string s, int inicio, int fim){
+  if (inicio >= fim){
     return true;
+  }
+  if (s[inicio] != s[fim]){
+    return false;
+  }
+  return ehpalidromo(s, inicio + 1, fim -1);
+  
 }
 
 
@@ -33,17 +30,14 @@ int main(){
     //std::cout<<ehpalidromo(resp)<<std::endl;
     //puxando a função para dar v ou f 
 
-    for (int i = 0; i < resp.length(); i++){
-        resp[i] = std::tolower(resp[i]);
+    for (char &c : resp){
+        c = std::tolower(c);
     }
-    if (ehpalidromo(resp)){
-        std::cout<<"É palindrome!"<<std::endl;
+    if (ehpalidromo(resp, 0, resp.length() - 1)){
+        std::cout<<"È palindrome"<<std::endl;
     }else{
-        std::cout<<"Não é palindrome!"<<std::endl;
+        std::cout<<"Não é palindrome"<<std::endl;
     }
-    //no primeiro for declaro os valores, no for eu declaro todo resultado 1 é palindrome, no else todo resultado 0 não é palindrome
-    
-    
 
     return 0;
 }
