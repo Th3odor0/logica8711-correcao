4a66615cd4c64f2b9fedbd47575b33109c2dcba7
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-06-02 19:42:09 -0300

    feat: adiciona verificador de palíndromo

--- 2main.cpp
+++ 2main.cpp
@@ -1,23 +1,49 @@
 #include<iostream>
 #include<Windows.h>
+#include<string>
+#include<cctype>
 
-int somarVetor(int vetor[], int indice){
-if (indice == 0){
-    return vetor[0];
-}
-return vetor[indice] + somarVetor(vetor, indice - 1);
+bool ehpalidromo(std::string s){
+    int inicio = 0;
+    int fim = s.length() - 1;
 
+    while (inicio < fim){
+        if (s[inicio] != s[fim]){
+            return false;
+        }
+        inicio++;
+        fim--;
+    }
+    return true;
 }
+
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int numeros[5] = {10, 20, 30, 40, 50};
+    std::string resp;
 
+  
+    
+    std::cout<<"Digite um palindrome"<<std::endl;
+    std::cin>>resp;
 
-    std::cout<<"Soma recursiva: "<<somarVetor(numeros, 4)<<std::endl;
+    //std::cout<<ehpalidromo(resp)<<std::endl;
+    //puxando a função para dar v ou f 
+
+    for (int i = 0; i < resp.length(); i++){
+        resp[i] = std::tolower(resp[i]);
+    }
+    if (ehpalidromo(resp)){
+        std::cout<<"É palindrome!"<<std::endl;
+    }else{
+        std::cout<<"Não é palindrome!"<<std::endl;
+    }
+    //no primeiro for declaro os valores, no for eu declaro todo resultado 1 é palindrome, no else todo resultado 0 não é palindrome
+    
+    
 
     return 0;
 }
