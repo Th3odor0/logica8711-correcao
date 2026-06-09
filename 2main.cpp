93493b1c3bda47bae260f7e3f8c514267dee5de0
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-06-09 20:23:25 -0300

    feat: cria estrutura base da todo list

--- 2main.cpp
+++ 2main.cpp
@@ -1,26 +1,99 @@
 #include<iostream>
 #include<string>
 #include<Windows.h>
+#include<vector>
 
-struct Aluno{
-    std::string nome;
-    float nota;
+
+
+struct Tarefa{
+    int id;
+    std::string descricao;
+    bool concluida;
 };
+ 
+Tarefa tarefas[50];
+    int totalTarefas = 0;
+ 
+    void adicionar(){
+        std::cout<<"\n --- Adicionar tarefas ---"<<std::endl;
+ 
+        std::cout<<"ID: ";
+        std::cin>>tarefas[totalTarefas].id;
+ 
+        std::cin.ignore();
+        std::cout<<"Descrição: ";
+        std::getline(std::cin, tarefas[totalTarefas].descricao);
+ 
+        tarefas[totalTarefas].concluida = false;
+ 
+        totalTarefas++;
+        std::cout<<"Tarefa adicionada!"<<std::endl;
+    }
+ 
+    void listar(){
+        if(totalTarefas == 0){
+            std::cout<<"\nNenhuma tarefa!"<<std::endl;
+            return;
+        }
+        std::cout<<"\n---- Tarefas ----"<<std::endl;
+        for (int i = 0; i < totalTarefas; i++){
+            std::string status = tarefas[i].concluida ? "S" : "N";
+            std::cout<<"["<<status<<"]"<<tarefas[i].id<<"-"<<tarefas[i].descricao<<std::endl;
+        }
+        
+    }
+    void maracarConcluida(){
+        int idBuscado;
+        std::cout<<"\nID da tarefa: ";
+        std::cin>>idBuscado;
 
+        for (int i = 0; i < totalTarefas; i++){
+            if (tarefas[i].id == idBuscado){
+                tarefas[i].concluida = true;
+                std::cout<<"Marcado como concluido"<<std::endl;
+                return;
+            }
+            
+        }
+        std::cout<<"Tarefa nao encontrada"<<std::endl;
+    }
+    void menu(){
+        std::cout<<"\n --- TODO LIST ==="<<std::endl;
+        std::cout<<"1. Adiociona"<<std::endl;
+          std::cout<<"2. Listar"<<std::endl;
+            std::cout<<"3. Marcar"<<std::endl;
+              std::cout<<"4. Sair"<<std::endl;
+                std::cout<<"Escolha: "<<std::endl;
+    }
 int main(){
-
+ 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+ 
+    int opcao;
 
-    Aluno alunos[3]={
-        {"Henrique", 8.5},
-        {"Paula", 9.0},
-        {"Juliana", 7.0}
-    };  
-    
-    for(int i = 0; i < 3; i++){
-        std::cout<<alunos[i].nome<<": "<<alunos[i].nota<<std::endl;
+    while (true){
+        menu();
+        std::cin>>opcao;
+
+        switch (opcao){
+        case 1:
+            adicionar();
+        break;
+                 case 2:
+            listar();
+        break;
+                 case 3:
+            maracarConcluida();
+        break;
+                 case 4:
+            std::cout<<"Até logo"<<std::endl;
+            return 0;
+         default:
+            std::cout<<"Opção inválida"<<std::endl;
+        }
     }
-
+ 
     return 0;
 }
+ 
