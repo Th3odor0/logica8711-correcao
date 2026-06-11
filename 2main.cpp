c165adfec8a6de5f28d4f78531284c9b404cb6d2
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-06-11 20:13:54 -0300

    BubbleSport

--- 2main.cpp
+++ 2main.cpp
@@ -2,26 +2,19 @@
 #include<windows.h>
 #include<string>
 
-void quickSort(int* arr, int esquerda, int direita){
-    if(esquerda >= direita)return;
-
-    int pivo = arr[direita];
-    int i = esquerda - 1;
-
-    for(int j = esquerda; j < direita; j++){
-        if(arr[j] < pivo){
-            i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+void bubbleSport(int* arr, int tamanho){
+    for (int i = 0; i < tamanho - 1; i++){
+        for (int j = 0; j < tamanho - i - 1; j++){
+            if (arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] =  temp;
+            }
+            
         }
+        
     }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[direita];
-    arr[direita] = temp;
-
-    quickSort(arr, esquerda, i);
-    quickSort(arr, i + 2, direita);
+    
 }
 
 int main(){
@@ -30,14 +23,18 @@ int main(){
     SetConsoleCP(CP_UTF8);
 
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int tamanho = 7;
-
-    quickSort(arr, 0, tamanho - 1);
-
-    for(int i = 0; i < tamanho; i++){
+    int tamanho = std::size(arr);
+    std::cout<<"Array original: ";
+    for (int i = 0; i < tamanho; i++){
         std::cout<<arr[i]<<" ";
     }
+    std::cout<<"\n";
 
-    
+    bubbleSport(arr, tamanho);
+    std::cout<<"Array ordenado: ";
+    for (int i = 0; i < tamanho; i++){
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<"\n";
     return 0;
 }
