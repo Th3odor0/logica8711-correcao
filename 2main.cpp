4d4c753302399caff3eb2ba6d15e1eee358c423b
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-06-11 19:40:29 -0300

    torre de hanoi

--- 2main.cpp
+++ 2main.cpp
@@ -2,19 +2,30 @@
 #include<string>
 #include<Windows.h>
 
-
+void torreDeHanoi(int n, std::string origem, std::string destino, std::string auxiliar){ 
+    if(n == 1){
+        std::cout<<"Mover disco "<<origem<<" para "<<destino<<std::endl;
+        return;
+    }
+    torreDeHanoi(n - 1, origem, auxiliar, destino);
+    std::cout<<"Mover disco "<<n<<" de "<<origem<<" para "<<destino<<std::endl;
+    torreDeHanoi(n - 1, auxiliar, destino, origem);
+    }
 
 int main(){
- 
+
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
- 
-    int idade =38;
 
-    std::string resultado = (idade >= 18) ? "Maior de idade" : "Menor de idade";
+    int numDiscos;
+    std::cout<<"Quantos discos: ";
+    std::cin>>numDiscos;
 
-    std::cout<<resultado<<std::endl;
-  
+    std::cout<<"\nSequência de movimentos: "<<std::endl;
+    torreDeHanoi(numDiscos, "A", "C", "B");
+
+    std::cout<<"\nTotal de movimento: "<<(1 << numDiscos) - 1<<std::endl;
+
+
     return 0;
 }
- 
