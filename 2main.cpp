4a41fde8367a1147285eb332a59ed2f1209fb0f1
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-06-17 19:35:09 -0300

    copa do mundo

--- 2main.cpp
+++ 2main.cpp
@@ -1,74 +1,51 @@
 #include<iostream>
-#include<windows.h>
 #include<string>
+#include<Windows.h>
 
+struct Jogador{
+    std::string nome;
+    std::string time;
+    int gols;
+};
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::string palavra = "prospeccao";
-    std::string adivinha = "";
-    int erros = 0;
-    int maxErros = 6;
-    bool ganhou = false;
-
-    for (int i = 0; i < palavra.length(); i++){
-        adivinha += "_";
-    }
+    Jogador jogadores[5];
     
-    std::cout<<"==== JOGP DA FORCA ===="<<std::endl;
-    std::cout<<"Advinhe a palavra"<<std::endl;
+    std::cout<<"==== COPA - RANKING DE GOLEADORES ===="<<std::endl;
     std::cout<<std::endl;
 
-    while (erros < maxErros && !ganhou){
-        std::cout<<"Palavra: ";
-        for (int i = 0; i < adivinha.length(); i++){
-            std::cout<<adivinha[i]<<"";
-        }
+    for(int i = 0; i < 5; i++){
+        std::cout<<"Jogador "<<(i + 1)<<":"<<std::endl;
+        std::cout<<"Nome: ";
+        std::cin>>jogadores[i].nome;
+        std::cout<<"Time: ";
+        std::cin>>jogadores[i].time;
+        std::cout<<"Gols: ";
+        std::cin>>jogadores[i].gols;
         std::cout<<std::endl;
-        std::cout<<"Erros: "<<erros<<"/"<<maxErros<<std::endl;
-        std::cout<<std::endl;
-        
-        char letras;
-        std::cout<<"Digite uma letra: ";
-        std::cin>>letras;
-        letras = std::tolower(letras);
-
-        std::cout<<std::endl;
-
-        bool encontrou = false;
-
-        for (int i = 0; i < palavra.length(); i++){
-            if (palavra[i] == letras){
-                adivinha[i] = letras;
-                encontrou = true;
-            }
-            
-        }
-        if (!encontrou){
-            std::cout<<"Letras errada!"<<std::endl;
-            erros++;
-        }else{
-            std::cout<<"Letra encontrada"<<std::endl;
-        }
-        std::cout<<std::endl;
-
-        if (adivinha == palavra){
-            ganhou = true;
-        }
-        
     }
-    std::cout<<"========="<<std::endl;
-    if (ganhou){
-        std::cout<<"Parabéns Você venceu"<<std::endl;
-        std::cout<<"A palavra era: "<<palavra<<std::endl;
-    }else{
-        std::cout<<"Game over Você perdeu"<<std::endl;
-        std::cout<<"A palavra era: "<<palavra<<std::endl;
+
+    std::cout<<"==== RANKING ===="<<std::endl;
+    std::cout<<std::endl;
+
+    for(int i = 0; i < 5 - 1; i++){
+        for(int j = 0; j < 5 - 1; j++){
+            if(jogadores[i].gols < jogadores[j + 1].gols){
+                Jogador temp = jogadores[j];
+                jogadores[j] = jogadores[j + 1];
+                jogadores[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 5; i++){
+        std::cout<<(i + 1)<<"º - "<<jogadores[i].nome
+                 <<" ("<<jogadores[i].time<<") - "
+                 <<jogadores[i].gols<<" gols"<<std::endl;
     }
     
-
     return 0;
 }
