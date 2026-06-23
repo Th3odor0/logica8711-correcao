ebd22f2431fbcfd1cae8ee9df8310498f0bcf473
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-06-23 20:52:38 -0300

    array com pares e impares

--- 2main.cpp
+++ 2main.cpp
@@ -5,27 +5,26 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int numeros[] = {1,2,3,2,4,5,3,6};
-    int tamanho = 8;
+    int numeros[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int tamanho = 10;
 
-    std::cout<<"==== NÚMEROS DUPLICADOS ===="<<std::endl;
+    std::cout<<"==== PARES E IMPARES ===="<<std::endl;
     std::cout<<std::endl;
 
-    std::cout<<"Array: ";
-    for(int i = 0; i < tamanho; i++){
+    std::cout<<"Pares: ";
+    for (int i = 0; i < tamanho; i++){
+      if (numeros[i] % 2 == 0){
         std::cout<<numeros[i]<<" ";
+      }  
     }
     std::cout<<std::endl;
 
-    std::cout<<"Numeros duplicados: "<<std::endl;
-    for(int i = 0; i < tamanho; i++){
-        for(int j = i + 1; j < tamanho; j++){
-            if(numeros[i] == numeros[j]){
-                std::cout<<numeros[i]<<" aparece mais de uma vez!"<<std::endl;
-                break;
-            }
-        }
+    std::cout<<"Ímpares: ";
+    for (int i = 0; i < tamanho; i++){
+      if (numeros[i] % 2 != 0){
+        std::cout<<numeros[i]<<" ";
+      }
+      
     }
-    
     return 0;
 }
