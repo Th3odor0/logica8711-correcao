b533e6fadb9274cfd6f7cb6c637b1c0f864007f8
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-07-02 20:09:48 -0300

    tamanho de vector

--- 2main.cpp
+++ 2main.cpp
@@ -6,25 +6,20 @@ int main(){
 
   std::vector<int>numeros;
 
-  std::cout<<"==== VECTOR (Arraylist em C++) ==="<<std::endl;
-
-  numeros.push_back(10);
-  numeros.push_back(20);
-  numeros.push_back(30);
-
-  std::cout<<"Numeros: ";
-  for (int i = 0; i < numeros.size(); i++){
-    std::cout<<numeros[i]<<" ";
-  }
+  std::cout<<"==== 2. Tamanho do vector ===="<<std::endl;
   std::cout<<std::endl;
 
-  std::cout<<"Tamanho: "<<numeros.size()<<std::endl;
-  numeros.pop_back();
+  numeros.push_back(5);
+  numeros.push_back(10);
+  numeros.push_back(15);
 
-  std::cout<<"Após remover o ultimo: ";
+  std::cout<<"Tamanho do vector: "<<numeros.size()<<std::endl;
+  std::cout<<"Elementos: ";
+
   for (int i = 0; i < numeros.size(); i++){
     std::cout<<numeros[i]<<" ";
   }
+  
   
 
   return 0;
