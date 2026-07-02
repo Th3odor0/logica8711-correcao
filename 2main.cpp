6f85b7ed57e793b7d9e5d5b70bd6c764af1184a5
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-07-02 20:42:56 -0300

    Remover o numero do vector

--- 2main.cpp
+++ 2main.cpp
@@ -6,22 +6,25 @@ int main(){
 
   std::vector<int>numeros;
 
-  std::cout<<"==== Acessar Elemento ===="<<std::endl;
+  std::cout<<"==== Remover Elemento especifico ===="<<std::endl;
 
-  numeros.push_back(100);
-  numeros.push_back(200);
-  numeros.push_back(300);
-  numeros.push_back(400);
+  numeros.push_back(10);
+  numeros.push_back(20);
+  numeros.push_back(30);
+  numeros.push_back(40);
 
-  std::cout<<"Vector: ";
+  std::cout<<"Antes: ";
   for (int i = 0; i < numeros.size(); i++){
-    std::cout<<numeros[i]<<std::endl;
+    std::cout<<numeros[i]<<" ";
   }
   std::cout<<std::endl;
 
-  std::cout<<"Elemento na posicao 0: "<<numeros[0]<<std::endl;
-  std::cout<<"Elemento na poiicao 2: "<<numeros[2]<<std::endl;
-  std::cout<<"Ultimo elemento: "<<numeros[3]<<std::endl;
+  numeros.erase(numeros.begin() + 1);
+
+  std::cout<<"Depois: ";
+  for (int i = 0; i < numeros.size(); i++){
+    std::cout<<numeros[i]<<" ";
+  }
   
   
 
