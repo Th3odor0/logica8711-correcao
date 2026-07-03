517e6bc0090dd3a8276fa275e363d91dead66500
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-07-02 21:27:59 -0300

    limpando vector

--- 2main.cpp
+++ 2main.cpp
@@ -6,26 +6,18 @@ int main(){
 
   std::vector<int>numeros;
 
-  std::cout<<"==== Remover Elemento especifico ===="<<std::endl;
+  std::cout<<"==== Limpar vector ===="<<std::endl;
 
   numeros.push_back(10);
   numeros.push_back(20);
   numeros.push_back(30);
-  numeros.push_back(40);
 
-  std::cout<<"Antes: ";
-  for (int i = 0; i < numeros.size(); i++){
-    std::cout<<numeros[i]<<" ";
-  }
-  std::cout<<std::endl;
-
-  numeros.erase(numeros.begin() + 1);
-
-  std::cout<<"Depois: ";
-  for (int i = 0; i < numeros.size(); i++){
-    std::cout<<numeros[i]<<" ";
-  }
+  std::cout<<"Tamanho Antes: "<<numeros.size()<<std::endl;
   
+  numeros.clear();
+
+  std::cout<<"Tamanho depois: "<<numeros.size()<<std::endl;
+  std::cout<<"Vector vazio?"<<(numeros.empty() ? "Sim" : "Nao")<<std::endl;
   
 
   return 0;
