454d0cf97bd86c2a5e0c020ec6e8c24f09e1e537
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-07-02 21:44:04 -0300

    maior e menor com vector

--- 2main.cpp
+++ 2main.cpp
@@ -6,18 +6,34 @@ int main(){
 
   std::vector<int>numeros;
 
-  std::cout<<"==== Limpar vector ===="<<std::endl;
+  std::cout<<"==== Maior e menor ===="<<std::endl;
 
-  numeros.push_back(10);
-  numeros.push_back(20);
-  numeros.push_back(30);
+  numeros.push_back(15);
+  numeros.push_back(8);
+  numeros.push_back(25);
+  numeros.push_back(1);
+  numeros.push_back(42);
 
-  std::cout<<"Tamanho Antes: "<<numeros.size()<<std::endl;
+  std::cout<<"Vector: ";
+  for (int i = 0; i < numeros.size(); i++){
+    std::cout<<numeros[i]<<" ";
+  }
+  std::cout<<std::endl;
+  int maior = numeros[0];
+  int menor = numeros[0];
+
+  for (int i = 0; i < numeros.size(); i++){
+      if (numeros[i] > maior){
+        maior = numeros[i];
+      }
+      if (numeros[i] < menor){
+        menor = numeros[i];
+      }
+      
+  }
   
-  numeros.clear();
-
-  std::cout<<"Tamanho depois: "<<numeros.size()<<std::endl;
-  std::cout<<"Vector vazio?"<<(numeros.empty() ? "Sim" : "Nao")<<std::endl;
+  std::cout<<"Maior: "<<maior<<std::endl;
+  std::cout<<"Menor: "<<menor<<std::endl;
   
 
   return 0;
