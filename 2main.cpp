44aeeef75b59c25558b09903738097755a222949
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-07-07 19:28:01 -0300

    somando numeros do vector

--- 2main.cpp
+++ 2main.cpp
@@ -6,35 +6,27 @@ int main(){
 
   std::vector<int>numeros;
 
-  std::cout<<"==== Maior e menor ===="<<std::endl;
+  numeros.push_back(10);
+  numeros.push_back(20);
+  numeros.push_back(30);
+  numeros.push_back(40);
+  
+  std::cout<<"Números:";
+  std::cout<<"Soma: ";
 
-  numeros.push_back(15);
-  numeros.push_back(8);
-  numeros.push_back(25);
-  numeros.push_back(1);
-  numeros.push_back(42);
-
-  std::cout<<"Vector: ";
   for (int i = 0; i < numeros.size(); i++){
     std::cout<<numeros[i]<<" ";
   }
-  std::cout<<std::endl;
-  int maior = numeros[0];
-  int menor = numeros[0];
 
+  int soma = 0;
   for (int i = 0; i < numeros.size(); i++){
-      if (numeros[i] > maior){
-        maior = numeros[i];
-      }
-      if (numeros[i] < menor){
-        menor = numeros[i];
-      }
-      
+    soma += numeros[i];
   }
+  std::cout<<std::endl;
+
+  std::cout<<"Somando Números: "<<soma<<std::endl;
   
-  std::cout<<"Maior: "<<maior<<std::endl;
-  std::cout<<"Menor: "<<menor<<std::endl;
-  
+
 
   return 0;
 };
