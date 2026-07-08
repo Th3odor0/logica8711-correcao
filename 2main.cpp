cb990a756256e2c73e09867b9e246407b414d114
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-07-07 21:14:03 -0300

    verificação de par e impar com vector

--- 2main.cpp
+++ 2main.cpp
@@ -9,20 +9,42 @@ int main(){
     SetConsoleCP(CP_UTF8);
 
     std::vector<int> numeros;
-    int quantidade;
+   
+    numeros.push_back(5);
+    numeros.push_back(10);
+    numeros.push_back(15);
+    numeros.push_back(20);
+    numeros.push_back(25);
 
-    std::cout<<"Adicionar do usuario"<<std::endl;
-
-    std::cout<<"Quantos numeros?"<<std::endl;
-    std::cin>>quantidade;
-
-    for (int i = 0; i < quantidade; i++){
-      int numero;
-      std::cout<<"Digite o numero "<<(i + 1)<<": ";
-      std::cin>>numero;
-      numeros.push_back(numero);
-    }
+    //apresente o  vector, mostre os numero...
+    //depois mostre só os pares e depois so os impares
     
 
+    //mostrando todos os numeros da tabela
+    std::cout<<"Mostrando todos os numeros: ";
+
+    for (int i = 0; i < numeros.size(); i++){
+      std::cout<<numeros[i]<<std::endl;
+    }
+    
+    //mostrando todos os numeros pares da tabela
+    std::cout<<"Mostrando Pares: ";
+
+    for (int i = 0; i < numeros.size(); i++){
+      std::cout<<numeros[i]<<std::endl;
+      if (numeros[i] % 2 == 0){
+        std::cout<<numeros[i]<<std::endl;
+      }
+    }
+    //mostrando todos os numeros impares da tabela
+    std::cout<<"Mostrando os impares: ";
+    
+    for (int i = 0; i < numeros.size(); i++){
+      std::cout<<numeros[i]<<std::endl;
+      if (numeros[i] % 2 !=0){
+        std::cout<<numeros[i]<<std::endl;
+      }
+      
+    }
     return 0;
 }
