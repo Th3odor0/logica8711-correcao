396783ea4213c5ce82d5c33ab9d28536cbd73055
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-07-07 21:39:25 -0300

    invertendo o vector

--- 2main.cpp
+++ 2main.cpp
@@ -9,41 +9,24 @@ int main(){
     SetConsoleCP(CP_UTF8);
 
     std::vector<int> numeros;
-   
-    numeros.push_back(5);
+
+    std::cout<<"Inverter o vector: "<<std::endl;
+
     numeros.push_back(10);
-    numeros.push_back(15);
     numeros.push_back(20);
-    numeros.push_back(25);
+    numeros.push_back(30);
+    numeros.push_back(40);
+    numeros.push_back(50);
 
-    //apresente o  vector, mostre os numero...
-    //depois mostre só os pares e depois so os impares
-    
-
-    //mostrando todos os numeros da tabela
-    std::cout<<"Mostrando todos os numeros: ";
-
-    for (int i = 0; i < numeros.size(); i++){
-      std::cout<<numeros[i]<<" ";
+    std::cout<<"Original: ";
+    for(int i = 0; i < numeros.size(); i++){
+        std::cout<<numeros[i]<<" ";
     }
     std::cout<<std::endl;
-    //mostrando todos os numeros pares da tabela
-    std::cout<<"Mostrando Pares: ";
-
-    for (int i = 0; i < numeros.size(); i++){
-      if (numeros[i] % 2 == 0){
+    std::cout<<"Invertido: ";
+    for(int i = numeros.size() - 1; i >= 0; i--){
         std::cout<<numeros[i]<<" ";
-      }
     }
     std::cout<<std::endl;
-    //mostrando todos os numeros impares da tabela
-    std::cout<<"Mostrando os impares: ";
-    
-    for (int i = 0; i < numeros.size(); i++){
-      if (numeros[i] % 2 !=0){
-        std::cout<<numeros[i]<<" ";
-      }
-      
-    }
     return 0;
 }
