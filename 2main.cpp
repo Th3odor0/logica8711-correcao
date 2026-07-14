48e816748459a91c0bfa473e7556eed442ecc3f1
THEODORO TERNUS DOS SANTOS <05109389063@senacrs.edu.br>
2026-07-14 19:39:03 -0300

    funções de calcular dobro e triplo

--- 2main.cpp
+++ 2main.cpp
@@ -1,6 +1,17 @@
-#include<iostream>
-#include<string>
-#include<Windows.h>
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <vector>
+
+
+double calcularDobro(double valor1 ){
+return valor1 * 2;
+};
+
+
+double calcularTriplo(double valor2){
+    return valor2 * 3;
+}
 
 
 
@@ -9,12 +20,17 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
  
-    int idade =38;
+   float numero;
+   
+   std::cout<<"Digite um numero: "<<std::endl;
+   std::cin>>numero;
 
-    std::string resultado = (idade >= 18) ? "Maior de idade" : "Menor de idade";
+    double resultado1 = calcularDobro(numero);
+    double resultado2 = calcularTriplo(numero);
 
-    std::cout<<resultado<<std::endl;
-  
+    std::cout<<"O dobro de "<<numero<<" e "<<resultado1<<std::endl;
+    std::cout<<"O triplo de "<<numero<<" e "<<resultado2<<std::endl;
+
+    
     return 0;
 }
- 
